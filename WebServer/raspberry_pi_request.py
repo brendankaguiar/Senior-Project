9da03b5daa940ca83c0046444d52b27cf8bd8986
brendankaguiar <brendankaguiar@gmail.com>
2021-12-04 20:52:50 -0800

    Parsed data into list elements, passed into json in raspberry request... -Brendan A.

--- WebServer/raspberry_pi_request.py
+++ WebServer/raspberry_pi_request.py
@@ -1,23 +1,32 @@
+#Reqests and Time functions written by Daniel B.
+#serial and time functions written by Brendan A.
+import serial
+from time import sleep
 import requests
 from datetime import datetime
 
 HOST = 'http://75.38.110.170:5000/'
 ROUTE = '/devicedata/all/0/'
-
 date = str(datetime.today().date())
 date = date.replace('-','_')
-
+now = datetime.now()
+current_timestamp = now.strftime("%H:%M:%S")
+port = serial.Serial("/dev/ttyS0",9600)#wait until data available
+Data = port.readline().decode("utf-8").strip()#decode data received to String
+W_Data = Data.split(",")#parse into list removing commas in the process
+#print(W_Data) #check data was parsed correctly
+#append W_Data to json
 #Replace variables after each : to your variables. Keep deviceid to 0
 try:
     req = requests.post(HOST+ROUTE+date,json={"timestamp": current_timestamp,
-                                              "date": d,
+                                              "date": date,
                                               "deviceid": 0,
-                                              "temperature": temp,
-                                              "windspeed": random.randint(0,100),
-                                              "winddirection": current_wind,
-                                              "humidity": round(random.uniform(0.45,0.55),2)*100,
-                                              "pressure": random.randint(0,100),
-                                              "aqi": aqi
+                                              "temperature": W_Data[0],
+                                              "windspeed": W_Data[1],
+                                              "winddirection": W_Data[2],
+                                              "humidity": W_Data[3],
+                                              "pressure": W_Data[4],
+                                              "aqi": W_Data[5]
                                               })
     print(req.content)
 except:

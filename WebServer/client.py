b76378df530f6d80694547fb9cc5c6756f8fe7d3
dbees15 <43460075+dbees15@users.noreply.github.com>
2022-05-03 21:52:10 -0700

    Moved some code from postclient.py to client.py

--- WebServer/client.py
+++ WebServer/client.py
@@ -8,6 +8,7 @@ import requests
 import time
 from datetime import datetime
 import random
+import math
 
 
 t = int(time.time())
@@ -20,6 +21,7 @@ d = d.replace('-','_')
 HOST = 'https://flask-rews.herokuapp.com/'
 #HOST = 'http://127.0.0.1:5000/'  # The server's hostname or IP address
 ROUTE = '/devicedata/metadata/0'
+#ROUTE = '/devicedata/all/0/'
 
 PORT = 5000        # The port used by the server
 #"GET /get/thingy HTTP/1.1\r\nHost:%s\r\n\r\n" % HOST
@@ -82,6 +84,44 @@ def postweather():
                                              "aqi": random.randint(0,100)
                                              })
         print(req.content.decode())
+
+wind_direction = ['N','NW','NE','S','SW','SE']
+
+def postnow():
+    t = datetime.now()
+    current_timestamp = t.timestamp()
+    d = str(t.date())
+    d = d.replace('-','_')
+
+    decimalhour = t.hour + (t.minute/60)
+
+    #get random wind direction:
+    current_wind = wind_direction[random.randint(0,5)]
+
+    #get random temp:
+    temp = 12*math.cos((t.hour+12)/(12/math.pi))+32 + random.uniform(-0.5,0.5)
+    temp = ((temp-32)*5)/9  #convert to celcius
+
+    wind_speed = 5 * math.cos((decimalhour+6)/(6/math.pi))+ 5.5 + random.uniform(-0.5,0.5)
+    aqi = 25+random.randint(-1,1)
+    aqi += random.randint(0,1)
+
+    try:
+        req = requests.post(HOST+ROUTE+d,json={"timestamp": int(current_timestamp),
+                                               "date": d,
+                                               "deviceid": device,
+                                               "temperature": round(temp,2),
+                                               "windspeed": round(wind_speed,2),
+                                               "winddirection": current_wind,
+                                               "humidity": int(round(random.uniform(0.45,0.55),2)*100),
+                                               "pressure": round(858.78 + round(random.uniform(-0.05,0.05),3),3),
+                                               "aqi": aqi
+                                               })
+        print(f"{current_timestamp}, {t}, {req.content.decode()}")
+    except:
+        print(f"Error Posting {current_timestamp}")
+    current_timestamp+=60
+
 
 def postdevice():
     for x in range(0,1):
